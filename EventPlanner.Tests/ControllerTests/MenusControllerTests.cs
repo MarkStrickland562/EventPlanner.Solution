46c4af8b3c4059a0c4528711b9b1a4661d740dd2
Mark Strickland <mstrickland@globys.com>
2019-03-09 19:01:13 -0800

    Add boilerplate files for Controller and Model tests.

---
+++ EventPlanner.Tests/ControllerTests/MenusControllerTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using EventPlanner.Controllers;
+using EventPlanner.Models;
+
+namespace EventPlanner.Tests
+{
+    [TestClass]
+    public class MenusControllerTest
+    {
+
+    }
+}
---
+++ EventPlanner.Tests/ModelTests/TaskTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EventPlanner.Models;
+using System.Collections.Generic;
+using System;
+
+namespace EventPlanner.Tests
+{
+  [TestClass]
+  public class TaskTest : IDisposable
+  {
+
+    public EventTest()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=event_planner_test;";
+    }
+
+    public void Dispose()
+    {
+      Event.ClearAll();
+    }
+
+
+  }
+}
