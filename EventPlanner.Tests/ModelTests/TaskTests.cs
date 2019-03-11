ea35ebc520294fe33515813ad3cb57a506a528e1
Mark Strickland <mstrickland@globys.com>
2019-03-11 14:15:53 -0700

    Add Task Constructor() method and test.

--- EventPlanner.Tests/ModelTests/TaskTests.cs
+++ EventPlanner.Tests/ModelTests/TaskTests.cs
@@ -15,9 +15,391 @@ namespace EventPlanner.Tests
 
     public void Dispose()
     {
-//      Event.ClearAll();
+//      Task.ClearAll();
+    }
+    [TestMethod]
+    public void TaskConstructor_CreatesInstanceOfTask_Task()
+    {
+      //Arrange, Act
+      string taskDescription = "Setup Tables";
+      DateTime taskPlannedStartDateTime = new DateTime(2019, 04, 04);
+      Task newTask = new Task(taskDescription, taskPlannedStartDateTime);
+
+      //Assert
+      Assert.AreEqual(typeof(Task), newTask.GetType());
     }
 
+    // [TestMethod]
+    // public void GetTaskName_ReturnsTaskName_String()
+    // {
+    //   //Arrange
+    //   string TaskName = "July 4th BBQ";
+    //   DateTime TaskDate = new DateTime(2019, 04, 04);
+    //   string TaskLocation = "Capitol Hill";
+    //   int menusId = 1;
+    //   Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId);
+    //
+    //   //Act
+    //   string result = newTask.GetTaskName();
+    //
+    //   //Assert
+    //   Assert.AreEqual(TaskName, result);
+    // }
+    //
+    // [TestMethod]
+    // public void SetTaskName_SetTaskName_String()
+    // {
+    //   //Arrange
+    //   string TaskName = "July 4th BBQ";
+    //   DateTime TaskDate = new DateTime(2019, 04, 04);
+    //   string TaskLocation = "Capitol Hill";
+    //   int menusId = 1;
+    //   Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId);
+    //
+    //   //Act
+    //   string updatedTaskName = "Birthday Party";
+    //   newTask.SetTaskName(updatedTaskName);
+    //   string result = newTask.GetTaskName();
+    //
+    //   //Assert
+    //   Assert.AreEqual(updatedTaskName, result);
+    // }
+    //
+    // [TestMethod]
+    // public void GetHireDate_ReturnsHireDate_DateTime()
+    // {
+    //   //Arrange
+    //   string TaskName = "July 4th BBQ";
+    //   DateTime TaskDate = new DateTime(2019, 04, 04);
+    //   string TaskLocation = "Capitol Hill";
+    //   int menusId = 1;
+    //   Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId);
+    //
+    //   //Act
+    //   DateTime result = newTask.GetTaskDate();
+    //
+    //   //Assert
+    //   Assert.AreEqual(TaskDate, result);
+    // }
+    //
+    // [TestMethod]
+    // public void SetTaskDate_SetTaskDate_DateTime()
+    // {
+    //   //Arrange
+    //   string TaskName = "July 4th BBQ";
+    //   DateTime TaskDate = new DateTime(2019, 04, 04);
+    //   string TaskLocation = "Capitol Hill";
+    //   int menusId = 1;
+    //   Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId);
+    //
+    //   //Act
+    //   DateTime updatedTaskDate = new DateTime(2019, 05, 03);
+    //   newTask.SetTaskDate(updatedTaskDate);
+    //   DateTime result = newTask.GetTaskDate();
+    //
+    //   //Assert
+    //   Assert.AreEqual(updatedTaskDate, result);
+    // }
+    //
+    // [TestMethod]
+    // public void GetTaskLocation_ReturnsTaskLocation_String()
+    // {
+    //   //Arrange
+    //   string TaskName = "July 4th BBQ";
+    //   DateTime TaskDate = new DateTime(2019, 04, 04);
+    //   string TaskLocation = "Capitol Hill";
+    //   int menusId = 1;
+    //   Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId);
+    //
+    //   //Act
+    //   string result = newTask.GetTaskLocation();
+    //
+    //   //Assert
+    //   Assert.AreEqual(TaskLocation, result);
+    // }
+    //
+    // [TestMethod]
+    // public void SetTaskLocation_SetTaskLocation_String()
+    // {
+    //   //Arrange
+    //   string TaskName = "July 4th BBQ";
+    //   DateTime TaskDate = new DateTime(2019, 04, 04);
+    //   string TaskLocation = "Capitol Hill";
+    //   int menusId = 1;
+    //   Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId);
+    //
+    //   //Act
+    //   string updatedTaskLocation = "Seattle";
+    //   newTask.SetTaskLocation(updatedTaskLocation);
+    //   string result = newTask.GetTaskLocation();
+    //
+    //   //Assert
+    //   Assert.AreEqual(updatedTaskLocation, result);
+    // }
+    //
+    // [TestMethod]
+    // public void GetMenusId_ReturnsMenusId_Int()
+    // {
+    //   //Arrange
+    //   string TaskName = "July 4th BBQ";
+    //   DateTime TaskDate = new DateTime(2019, 04, 04);
+    //   string TaskLocation = "Capitol Hill";
+    //   int menusId = 1;
+    //   Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId);
+    //
+    //   //Act
+    //   int result = newTask.GetMenusId();
+    //
+    //   //Assert
+    //   Assert.AreEqual(menusId, result);
+    // }
+    //
+    // [TestMethod]
+    // public void SetMenusId_SetMenusId_Int()
+    // {
+    //   //Arrange
+    //   string TaskName = "July 4th BBQ";
+    //   DateTime TaskDate = new DateTime(2019, 04, 04);
+    //   string TaskLocation = "Capitol Hill";
+    //   int menusId = 1;
+    //   Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId);
+    //
+    //   //Act
+    //   int updatedMenusId = 2;
+    //   newTask.SetMenusId(updatedMenusId);
+    //   int result = newTask.GetMenusId();
+    //
+    //   //Assert
+    //   Assert.AreEqual(updatedMenusId, result);
+    // }
+    //
+    // [TestMethod]
+    // public void GetId_ReturnsTaskId_Int()
+    // {
+    //   //Arrange
+    //   string TaskName = "July 4th BBQ";
+    //   DateTime TaskDate = new DateTime(2019, 04, 04);
+    //   string TaskLocation = "Capitol Hill";
+    //   int menusId = 1;
+    //   Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId);
+    //
+    //   //Act
+    //   int result = newTask.GetId();
+    //
+    //   //Assert
+    //   Assert.AreEqual(0, result);
+    // }
+    //
+    // [TestMethod]
+    // public void Save_SavesTaskToDatabase_TaskList()
+    // {
+    //   //Arrange
+    //   string TaskName = "July 4th BBQ";
+    //   DateTime TaskDate = new DateTime(2019, 04, 04);
+    //   string TaskLocation = "Capitol Hill";
+    //   int menusId = 1;
+    //   Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId);
+    //   newTask.Save();
+    //
+    //   //Act
+    //   List<Task> result = Task.GetAll();
+    //   List<Task> testList = new List<Task>{newTask};
+    //
+    //   //Assert
+    //   CollectionAssert.AreEqual(testList, result);
+    // }
+    //
+    // [TestMethod]
+    // public void GetAll_ReturnsAllTaskObjects_TaskList()
+    // {
+    //   //Arrange
+    //   string TaskName1 = "July 4th BBQ";
+    //   DateTime TaskDate1 = new DateTime(2019, 04, 04);
+    //   string TaskLocation1 = "Capitol Hill";
+    //   int menusId1 = 1;
+    //   Task newTask1 = new Task(TaskName1, TaskDate1, TaskLocation1, menusId1);
+    //   newTask1.Save();
+    //
+    //   string TaskName2 = "Birthday Party";
+    //   DateTime TaskDate2 = new DateTime(2019, 05, 03);
+    //   string TaskLocation2 = "Saltys";
+    //   int menusId2 = 2;
+    //   Task newTask2 = new Task(TaskName2, TaskDate2, TaskLocation2, menusId2);
+    //   newTask2.Save();
+    //
+    //   List<Task> newList = new List<Task> { newTask1, newTask2};
+    //
+    //   //Act
+    //   List<Task> result = Task.GetAll();
+    //
+    //   //Assert
+    //   CollectionAssert.AreEqual(newList, result);
+    // }
+    //
+    // [TestMethod]
+    // public void Find_ReturnsTaskInDatabase_Task()
+    // {
+    //   //Arrange
+    //   string TaskName = "July 4th BBQ";
+    //   DateTime TaskDate = new DateTime(2019, 04, 04);
+    //   string TaskLocation = "Capitol Hill";
+    //   int menusId = 1;
+    //   Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId);
+    //   newTask.Save();
+    //
+    //   //Act
+    //   Task foundTask = Task.Find(newTask.GetId());
+    //
+    //   //Assert
+    //   Assert.AreEqual(newTask, foundTask);
+    // }
 
+    // [TestMethod]
+    // public void GetTasks_RetrievesAllTasksWithTask_TaskList()
+    // {
+    //   //Arrange, Act
+    //   string name = "Betty Clark";
+    //   DateTime hireDate = new DateTime(2019, 01, 01);
+    //   Task newTask = new Task(name, hireDate);
+    //   newTask.Save();
+    //
+    //   string name1 = "Tom Jones";
+    //   string gender1 = "Male";
+    //   int TaskId1 = newTask.GetId();
+    //   Task newTask1 = new Task(name1, gender1, TaskId1);
+    //   newTask1.Save();
+    //
+    //   string name2 = "Jane Doe";
+    //   string gender2 = "Female";
+    //   int TaskId2 = newTask.GetId();
+    //   Task newTask2 = new Task(name2, gender2, TaskId2);
+    //   newTask2.Save();
+    //
+    //   List<Task> newList = new List<Task> { newTask1, newTask2 };
+    //
+    //   List<Task> resultList = newTask.GetTasks();
+    //
+    //   //Assert
+    //   CollectionAssert.AreEqual(newList, resultList);
+    // }
+    //
+    // [TestMethod]
+    // public void Delete_DeletesTaskFromDatabase()
+    // {
+    //   //Arrange
+    //   string TaskName = "July 4th BBQ";
+    //   DateTime TaskDate = new DateTime(2019, 04, 04);
+    //   string TaskLocation = "Capitol Hill";
+    //   int menusId = 1;
+    //   Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId);
+    //   newTask.Save();
+    //   newTask.Delete();
+    //
+    //   //Act
+    //   List<Task> newList = new List<Task> { newTask };
+    //   List<Task> resultList = Task.GetAll();
+    //
+    //   //Assert
+    //   CollectionAssert.AreNotEqual(newList, resultList);
+    // }
+    //
+    // [TestMethod]
+    // public void DeleteAll_DeletesAllTasksFromDatabase()
+    // {
+    //   //Arrange
+    //   string TaskName = "July 4th BBQ";
+    //   DateTime TaskDate = new DateTime(2019, 04, 04);
+    //   string TaskLocation = "Capitol Hill";
+    //   int menusId = 1;
+    //   Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId);
+    //   newTask.Save();
+    //   Task.DeleteAll();
+    //
+    //   //Act
+    //   List<Task> newList = new List<Task> { newTask };
+    //   List<Task> resultList = Task.GetAll();
+    //
+    //   //Assert
+    //   CollectionAssert.AreNotEqual(newList, resultList);
+    // }
+    //
+    // [TestMethod]
+    // public void Edit_UpdatesTaskToDatabase()
+    // {
+    //   //Arrange
+    //   string TaskName = "July 4th BBQ";
+    //   DateTime TaskDate = new DateTime(2019, 04, 04);
+    //   string TaskLocation = "Capitol Hill";
+    //   int menusId = 1;
+    //   Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId);
+    //   newTask.Save();
+    //
+    //   //Act
+    //   Task foundTask = Task.Find(newTask.GetId());
+    //   string newName = "Birthday Party";
+    //   DateTime newTaskDate = new DateTime(2019, 05, 03);
+    //   string newTaskLocation = "Saltys";
+    //   int newMenusId = 2;
+    //   foundTask.Edit(newName, newTaskDate, newTaskLocation, newMenusId);
+    //   Task updatedTask = Task.Find(newTask.GetId());
+    //
+    //   List<Task> result = Task.GetAll();
+    //   List<Task> testList = new List<Task>{updatedTask};
+    //
+    //   //Assert
+    //   CollectionAssert.AreEqual(testList, result);
+    // }
+
+    // [TestMethod]
+    // public void Save_SavesTaskSpecialtyToDatabase_SpecialtyList()
+    // {
+    //   //Arrange
+    //   string name = "Betty Clark";
+    //   DateTime hireDate = new DateTime(2019, 01, 01);
+    //   Task newTask = new Task(name, hireDate);
+    //   newTask.Save();
+    //   string specialty = "Colorist";
+    //   Specialty newSpecialty = new Specialty(specialty);
+    //   newSpecialty.Save();
+    //
+    //   //Act
+    //   Task foundTask = Task.Find(newTask.GetId());
+    //   Specialty foundSpecialty = Specialty.Find(newSpecialty.GetId());
+    //   foundTask.AddSpecialty(foundSpecialty);
+    //
+    //   List<Specialty> result = newTask.GetSpecialties();
+    //   List<Specialty> testList = new List<Specialty>{foundSpecialty};
+    //
+    //   //Assert
+    //   CollectionAssert.AreEqual(testList, result);
+    // }
+    //
+    // [TestMethod]
+    // public void GetSpecialties_RetrievesAllSpecialtiesForATask_SpecialtyList()
+    // {
+    //   //Arrange
+    //   string name = "Betty Clark";
+    //   DateTime hireDate = new DateTime(2019, 01, 01);
+    //   Task newTask = new Task(name, hireDate);
+    //   newTask.Save();
+    //   string specialty1 = "Colorist";
+    //   Specialty newSpecialty1 = new Specialty(specialty1);
+    //   newSpecialty1.Save();
+    //   string specialty2 = "Barber";
+    //   Specialty newSpecialty2 = new Specialty(specialty2);
+    //   newSpecialty2.Save();
+    //   //Act
+    //   Task foundTask = Task.Find(newTask.GetId());
+    //   Specialty foundSpecialty1 = Specialty.Find(newSpecialty1.GetId());
+    //   Specialty foundSpecialty2 = Specialty.Find(newSpecialty2.GetId());
+    //   foundTask.AddSpecialty(foundSpecialty1);
+    //   foundTask.AddSpecialty(foundSpecialty2);
+    //
+    //   List<Specialty> result = newTask.GetSpecialties();
+    //   List<Specialty> testList = new List<Specialty>{foundSpecialty1, foundSpecialty2};
+    //
+    //   //Assert
+    //   CollectionAssert.AreEqual(testList, result);
+    // }
   }
 }
--- EventPlanner/Models/Task.cs
+++ EventPlanner/Models/Task.cs
@@ -6,6 +6,339 @@ namespace EventPlanner.Models
 {
   public class Task
   {
+    private string _taskDescription;
+    private DateTime _taskPlannedStartDateTime;
+    private int _id;
 
+    public Task(string taskDescription, DateTime taskPlannedStartDateTime, int id = 0)
+    {
+      _taskDescription = taskDescription;
+      _taskPlannedStartDateTime = taskPlannedStartDateTime;
+      _id = id;
+    }
+
+    // public string GetTaskName()
+    // {
+    //   return _TaskName;
+    // }
+    //
+    // public void SetTaskName(string newTaskName)
+    // {
+    //   _TaskName = newTaskName;
+    // }
+    //
+    // public DateTime GetTaskDate()
+    // {
+    //   return _TaskDate;
+    // }
+    //
+    // public void SetTaskDate(DateTime newTaskDate)
+    // {
+    //   _TaskDate = newTaskDate;
+    // }
+    //
+    // public string GetTaskLocation()
+    // {
+    //   return _TaskLocation;
+    // }
+    //
+    // public void SetTaskLocation(string newTaskLocation)
+    // {
+    //   _TaskLocation = newTaskLocation;
+    // }
+    //
+    // public int GetMenusId()
+    // {
+    //   return _menusId;
+    // }
+    //
+    // public void SetMenusId(int newMenusId)
+    // {
+    //   _menusId = newMenusId;
+    // }
+    //
+    // public int GetId()
+    // {
+    //   return _id;
+    // }
+    //
+    // public static void ClearAll()
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"DELETE FROM Tasks_tasks; DELETE FROM Tasks_invitees; DELETE FROM Tasks;";
+    //   cmd.ExecuteNonQuery();
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    // }
+    //
+    // public void Save()
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"INSERT INTO Tasks (name, Task_date, Task_location, menus_id)
+    //                       VALUES (@TaskName, @TaskDate, @TaskLocation, @menusId);";
+    //   MySqlParameter TaskName = new MySqlParameter();
+    //   TaskName.ParameterName = "@TaskName";
+    //   TaskName.Value = this._TaskName;
+    //   cmd.Parameters.Add(TaskName);
+    //   MySqlParameter TaskDate = new MySqlParameter();
+    //   TaskDate.ParameterName = "@TaskDate";
+    //   TaskDate.Value = this._TaskDate;
+    //   cmd.Parameters.Add(TaskDate);
+    //   MySqlParameter TaskLocation = new MySqlParameter();
+    //   TaskLocation.ParameterName = "@TaskLocation";
+    //   TaskLocation.Value = this._TaskLocation;
+    //   cmd.Parameters.Add(TaskLocation);
+    //   MySqlParameter menusId = new MySqlParameter();
+    //   menusId.ParameterName = "@menusId";
+    //   menusId.Value = this._menusId;
+    //   cmd.Parameters.Add(menusId);
+    //   cmd.ExecuteNonQuery();
+    //   _id = (int) cmd.LastInsertedId;
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    // }
+    //
+    // public static List<Task> GetAll()
+    // {
+    //   List<Task> allTasks = new List<Task> {};
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"SELECT * FROM Tasks;";
+    //   var rdr = cmd.ExecuteReader() as MySqlDataReader;
+    //   while(rdr.Read())
+    //   {
+    //     int TaskId = rdr.GetInt32(0);
+    //     string TaskName = rdr.GetString(1);
+    //     DateTime TaskDate = rdr.GetDateTime(2);
+    //     string TaskLocation = rdr.GetString(3);
+    //     int menusId = rdr.GetInt32(4);
+    //     Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId, TaskId);
+    //     allTasks.Add(newTask);
+    //   }
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    //   return allTasks;
+    // }
+    //
+    // public static Task Find(int id)
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"SELECT * FROM Tasks WHERE id = (@searchId);";
+    //   MySqlParameter searchId = new MySqlParameter();
+    //   searchId.ParameterName = "@searchId";
+    //   searchId.Value = id;
+    //   cmd.Parameters.Add(searchId);
+    //   var rdr = cmd.ExecuteReader() as MySqlDataReader;
+    //   int TaskId = 0;
+    //   string TaskName = "";
+    //   DateTime TaskDate = new DateTime(1900, 1, 1);
+    //   string TaskLocation = "";
+    //   int menusId = 0;
+    //   while(rdr.Read())
+    //   {
+    //     TaskId = rdr.GetInt32(0);
+    //     TaskName = rdr.GetString(1);
+    //     TaskDate = rdr.GetDateTime(2);
+    //     TaskLocation = rdr.GetString(3);
+    //     menusId = rdr.GetInt32(4);
+    //   }
+    //   Task newTask = new Task(TaskName, TaskDate, TaskLocation, menusId, TaskId);
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    //     return newTask;
+    // }
+    //
+    // // public List<Task> GetTasks()
+    // // {
+    // //   List<Task> allTaskTasks = new List<Task> {};
+    // //   MySqlConnection conn = DB.Connection();
+    // //   conn.Open();
+    // //   var cmd = conn.CreateCommand() as MySqlCommand;
+    // //   cmd.CommandText = @"SELECT *
+    // //                         FROM tasks
+    // //                        WHERE task_id IN (SELECT task_id FROM Tasks_tasks WHERE Tasks_id = @Task_id);";
+    // //   MySqlParameter TaskId = new MySqlParameter();
+    // //   TaskId.ParameterName = "@Task_id";
+    // //   TaskId.Value = this._id;
+    // //   cmd.Parameters.Add(TaskId);
+    // //   var rdr = cmd.ExecuteReader() as MySqlDataReader;
+    // //   while(rdr.Read())
+    // //   {
+    // //     int taskId = rdr.GetInt32(0);
+    // //     string taskDescription = rdr.GetString(1);
+    // //     DateTime plannedStartDateTime = rdr.GetDateTime(2);
+    // //     Task newTask = new Task(taskDescription, plannedStartDateTime, taskId);
+    // //     allTaskTasks.Add(newTask);
+    // //   }
+    // //   conn.Close();
+    // //   if (conn != null)
+    // //   {
+    // //     conn.Dispose();
+    // //   }
+    // //   return allTaskTasks;
+    // // }
+    //
+    // public void Delete()
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"DELETE FROM Tasks_tasks WHERE Tasks_id = (@searchid);
+    //                       DELETE FROM Tasks_invitees WHERE Tasks_id = (@searchId);
+    //                       DELETE FROM Tasks WHERE id = (@searchId);";
+    //   MySqlParameter searchId = new MySqlParameter();
+    //   searchId.ParameterName = "@searchId";
+    //   searchId.Value = this._id;
+    //   cmd.Parameters.Add(searchId);
+    //   cmd.ExecuteNonQuery();
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    // }
+    //
+    // public static void DeleteAll()
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"DELETE FROM Tasks_tasks;
+    //                       DELETE FROM Tasks_invitees;
+    //                       DELETE FROM Tasks";
+    //   cmd.ExecuteNonQuery();
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    // }
+    //
+    //   public void Edit(string newTaskName, DateTime newTaskDate, string newTaskLocation, int newMenusId)
+    //   {
+    //     MySqlConnection conn = DB.Connection();
+    //     conn.Open();
+    //     var cmd = conn.CreateCommand() as MySqlCommand;
+    //     cmd.CommandText = @"UPDATE Tasks SET name = (@TaskName), Task_date = (@TaskDate), Task_location = (@TaskLocation), menus_id = (@menusId)
+    //                          WHERE id = (@TaskId);";
+    //     MySqlParameter TaskNameParameter = new MySqlParameter();
+    //     TaskNameParameter.ParameterName = "@TaskName";
+    //     TaskNameParameter.Value = newTaskName;
+    //     cmd.Parameters.Add(TaskNameParameter);
+    //     MySqlParameter TaskDateParameter = new MySqlParameter();
+    //     TaskDateParameter.ParameterName = "@TaskDate";
+    //     TaskDateParameter.Value = newTaskDate;
+    //     cmd.Parameters.Add(TaskDateParameter);
+    //     MySqlParameter TaskLocationParameter = new MySqlParameter();
+    //     TaskLocationParameter.ParameterName = "@TaskLocation";
+    //     TaskLocationParameter.Value = newTaskLocation;
+    //     cmd.Parameters.Add(TaskLocationParameter);
+    //     MySqlParameter menusIdParameter = new MySqlParameter();
+    //     menusIdParameter.ParameterName = "@menusId";
+    //     menusIdParameter.Value = newMenusId;
+    //     cmd.Parameters.Add(menusIdParameter);
+    //     MySqlParameter TaskIdParameter = new MySqlParameter();
+    //     TaskIdParameter.ParameterName = "@TaskId";
+    //     TaskIdParameter.Value = this._id;
+    //     cmd.Parameters.Add(TaskIdParameter);
+    //     cmd.ExecuteNonQuery();
+    //     _TaskName = newTaskName;
+    //     _TaskDate = newTaskDate;
+    //     _TaskLocation = newTaskLocation;
+    //     _menusId = newMenusId;
+    //     conn.Close();
+    //     if (conn != null)
+    //     {
+    //       conn.Dispose();
+    //     }
+    //   }
+
+      // public void AddSpecialty(Specialty newSpecialty)
+      // {
+      //   MySqlConnection conn = DB.Connection();
+      //   conn.Open();
+      //   var cmd = conn.CreateCommand() as MySqlCommand;
+      //   cmd.CommandText = @"INSERT INTO Tasks_specialties (Tasks_id, specialties_id) VALUES (@TaskId, @specialtyId);";
+      //   MySqlParameter specialtyIdParameter = new MySqlParameter();
+      //   specialtyIdParameter.ParameterName = "@specialtyId";
+      //   specialtyIdParameter.Value = newSpecialty.GetId();
+      //   cmd.Parameters.Add(specialtyIdParameter);
+      //   MySqlParameter TaskIdParameter = new MySqlParameter();
+      //   TaskIdParameter.ParameterName = "@TaskId";
+      //   TaskIdParameter.Value = this._id;
+      //   cmd.Parameters.Add(TaskIdParameter);
+      //   cmd.ExecuteNonQuery();
+      //   conn.Close();
+      //   if (conn != null)
+      //   {
+      //     conn.Dispose();
+      //   }
+      // }
+      //
+      // public List<Specialty> GetSpecialties()
+      // {
+      //   MySqlConnection conn = DB.Connection();
+      //   conn.Open();
+      //   var cmd = conn.CreateCommand() as MySqlCommand;
+      //   cmd.CommandText = @"SELECT specialties.*
+      //                         FROM Tasks
+      //                         JOIN Tasks_specialties ON (Tasks.id = Tasks_specialties.Tasks_id)
+      //                         JOIN specialties ON (Tasks_specialties.specialties_id = specialties.id)
+      //                        WHERE Tasks.id = (@TaskId);";
+      //   MySqlParameter TaskIdParameter = new MySqlParameter();
+      //   TaskIdParameter.ParameterName = "@TaskId";
+      //   TaskIdParameter.Value = this._id;
+      //   cmd.Parameters.Add(TaskIdParameter);
+      //   MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      //   List<Specialty> specialtyList = new List<Specialty>{};
+      //   while(rdr.Read())
+      //   {
+      //     int specialtyId = rdr.GetInt32(0);
+      //     string specialty= rdr.GetString(1);
+      //     Specialty newSpecialty = new Specialty(specialty, specialtyId);
+      //     specialtyList.Add(newSpecialty);
+      //   }
+      //   conn.Close();
+      //   if (conn != null)
+      //   {
+      //      conn.Dispose();
+      //   }
+      //   return specialtyList;
+      // }
+
+    // public override bool Equals(System.Object otherTask)
+    // {
+    //   if (!(otherTask is Task))
+    //   {
+    //     return false;
+    //   }
+    //   else
+    //   {
+    //     Task newTask = (Task) otherTask;
+    //     bool idEquality = this.GetId().Equals(newTask.GetId());
+    //     bool taskDescriptionEquality = this.GetTaskDescription().Equals(newTask.GetTaskDescription());
+    //     bool taskPlannedStartDateTimeEquality = this.GetTaskPlannedStartDateTime().Equals(newTask.GetTaskPlannedStartDateTime());
+    //     return (idEquality && taskDescriptionEquality && taskPlannedStartDateTimeEquality);
+    //   }
+    // }
   }
 }
