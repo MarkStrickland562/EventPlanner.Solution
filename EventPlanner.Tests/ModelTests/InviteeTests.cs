48dd22754bf0b8d9546ecea7411434e4a78c6dde
Mark Strickland <mstrickland@globys.com>
2019-03-11 18:49:41 -0700

    Add Invitee Controller and test.

--- EventPlanner.Tests/ModelTests/InviteeTests.cs
+++ EventPlanner.Tests/ModelTests/InviteeTests.cs
@@ -18,6 +18,276 @@ namespace EventPlanner.Tests
 //      Event.ClearAll();
     }
 
+    [TestMethod]
+    public void inviteeConstructor_CreatesInstanceOfInvitee_Invitee()
+    {
+      //Arrange, Act
+      string inviteeName = "Jane Doe";
+      string inviteeEmailAddress = "janedoe@mail.com";
+      Invitee newInvitee = new Invitee(inviteeName, inviteeEmailAddress);
 
+      //Assert
+      Assert.AreEqual(typeof(Invitee), newInvitee.GetType());
+    }
+
+    // [TestMethod]
+    // public void GetinviteeName_ReturnsinviteeName_String()
+    // {
+    //   //Arrange
+    //   string inviteeName = "Jane Doe";
+    //   string inviteeEmailAddress = "janedoe@mail.com";
+    //   invitee newinvitee = new invitee(inviteeName, inviteeEmailAddress);
+    //
+    //   //Act
+    //   string result = newinvitee.GetinviteeName();
+    //
+    //   //Assert
+    //   Assert.AreEqual(inviteeName, result);
+    // }
+    //
+    // [TestMethod]
+    // public void SetinviteeName_SetinviteeName_String()
+    // {
+    //   //Arrange
+    //   string inviteeName = "Jane Doe";
+    //   string inviteeEmailAddress = "janedoe@mail.com";
+    //   invitee newinvitee = new invitee(inviteeName, inviteeEmailAddress);
+    //
+    //   //Act
+    //   string updatedinviteeName = "John Smith";
+    //   newinvitee.SetinviteeName(updatedinviteeName);
+    //   string result = newinvitee.GetinviteeName();
+    //
+    //   //Assert
+    //   Assert.AreEqual(updatedinviteeName, result);
+    // }
+    //
+    // [TestMethod]
+    // public void GetinviteeEmailAddress_ReturnsinviteeEmailAddress_string()
+    // {
+    //   //Arrange
+    //   string inviteeName = "Jane Doe";
+    //   string inviteeEmailAddress = "janedoe@mail.com";
+    //   invitee newinvitee = new invitee(inviteeName, inviteeEmailAddress);
+    //
+    //   //Act
+    //   string result = newinvitee.GetinviteeEmailAddress();
+    //
+    //   //Assert
+    //   Assert.AreEqual(inviteeEmailAddress, result);
+    // }
+    //
+    // [TestMethod]
+    // public void SetinviteeEmailAddress_SetinviteeEmailAddress_string()
+    // {
+    //   //Arrange
+    //   string inviteeName = "Jane Doe";
+    //   string inviteeEmailAddress = "janedoe@mail.com";
+    //   invitee newinvitee = new invitee(inviteeName, inviteeEmailAddress);
+    //
+    //   //Act
+    //   string updatedinviteeEmailAddress = new string(2019, 05, 03);
+    //   newinvitee.SetinviteeEmailAddress(updatedinviteeEmailAddress);
+    //   string result = newinvitee.GetinviteeEmailAddress();
+    //
+    //   //Assert
+    //   Assert.AreEqual(updatedinviteeEmailAddress, result);
+    // }
+    //
+    // [TestMethod]
+    // public void GetId_ReturnsinviteeId_Int()
+    // {
+    //   //Arrange
+    //   string inviteeName = "Jane Doe";
+    //   string inviteeEmailAddress = "janedoe@mail.com";
+    //   invitee newinvitee = new invitee(inviteeName, inviteeEmailAddress);
+    //
+    //   //Act
+    //   int result = newinvitee.GetId();
+    //
+    //   //Assert
+    //   Assert.AreEqual(0, result);
+    // }
+    //
+    // [TestMethod]
+    // public void Save_SavesinviteeToDatabase_inviteeList()
+    // {
+    //   //Arrange
+    //   string inviteeName = "Jane Doe";
+    //   string inviteeEmailAddress = "janedoe@mail.com";
+    //   invitee newinvitee = new invitee(inviteeName, inviteeEmailAddress);
+    //   newinvitee.Save();
+    //
+    //   //Act
+    //   List<invitee> result = invitee.GetAll();
+    //   List<invitee> testList = new List<invitee>{newinvitee};
+    //
+    //   //Assert
+    //   CollectionAssert.AreEqual(testList, result);
+    // }
+    //
+    // [TestMethod]
+    // public void GetAll_ReturnsAllinviteeObjects_inviteeList()
+    // {
+    //   //Arrange
+    //   string inviteeName1 = "Jane Doe";
+    //   string inviteeEmailAddress1 = "janedoe@mail.com";
+    //   invitee newinvitee1 = new invitee(inviteeName1, inviteeEmailAddress1);
+    //   newinvitee1.Save();
+    //
+    //   string inviteeName2 = "John Smith";
+    //   string inviteeEmailAddress2 = "janedoe@mail.com";
+    //   invitee newinvitee2 = new invitee(inviteeName2, inviteeEmailAddress2);
+    //   newinvitee2.Save();
+    //
+    //   List<invitee> newList = new List<invitee> { newinvitee1, newinvitee2};
+    //
+    //   //Act
+    //   List<invitee> result = invitee.GetAll();
+    //
+    //   //Assert
+    //   CollectionAssert.AreEqual(newList, result);
+    // }
+    //
+    // [TestMethod]
+    // public void Find_ReturnsinviteeInDatabase_invitee()
+    // {
+    //   //Arrange
+    //   string inviteeName = "Jane Doe";
+    //   string inviteeEmailAddress = "janedoe@mail.com";
+    //   invitee newinvitee = new invitee(inviteeName, inviteeEmailAddress);
+    //   newinvitee.Save();
+    //
+    //   //Act
+    //   invitee foundinvitee = invitee.Find(newinvitee.GetId());
+    //
+    //   //Assert
+    //   Assert.AreEqual(newinvitee, foundinvitee);
+    // }
+    //
+    // [TestMethod]
+    // public void GetEvents_RetrievesAllEventsWithinvitee_EventList()
+    // {
+    //   //Arrange
+    //   string eventName1 = "July 4th BBQ";
+    //   string eventDate1 = "janedoe@mail.com";
+    //   string eventLocation1 = "Capitol Hill";
+    //   int menusId1 = 1;
+    //   Event newEvent1 = new Event(eventName1, eventDate1, eventLocation1, menusId1);
+    //   newEvent1.Save();
+    //
+    //   string eventName2 = "Birthday Party";
+    //   string eventDate2 = new string(2019, 05, 03);
+    //   string eventLocation2 = "Saltys";
+    //   int menusId2 = 2;
+    //   Event newEvent2 = new Event(eventName2, eventDate2, eventLocation2, menusId2);
+    //   newEvent2.Save();
+    //
+    //   string inviteeName = "Jane Doe";
+    //   string inviteeEmailAddress = "janedoe@mail.com";
+    //   invitee newinvitee = new invitee(inviteeName, inviteeEmailAddress);
+    //   newinvitee.Save();
+    //
+    //   //Act
+    //   Event foundEvent1 = Event.Find(newEvent1.GetId());
+    //   Event foundEvent2 = Event.Find(newEvent2.GetId());
+    //   invitee foundinvitee = invitee.Find(newinvitee.GetId());
+    //
+    //   foundinvitee.AddEvent(foundEvent1);
+    //   foundinvitee.AddEvent(foundEvent2);
+    //
+    //   List<Event> newList = new List<Event> { newEvent1, newEvent2 };
+    //   List<Event> resultList = foundinvitee.GetEvents();
+    //
+    //   //Assert
+    //   CollectionAssert.AreEqual(newList, resultList);
+    // }
+    //
+    // [TestMethod]
+    // public void Delete_DeletesinviteeFromDatabase()
+    // {
+    //   //Arrange
+    //   string inviteeName = "Jane Doe";
+    //   string inviteeEmailAddress = "janedoe@mail.com";
+    //   invitee newinvitee = new invitee(inviteeName, inviteeEmailAddress);
+    //   newinvitee.Save();
+    //   newinvitee.Delete();
+    //
+    //   //Act
+    //   List<invitee> newList = new List<invitee> { newinvitee };
+    //   List<invitee> resultList = invitee.GetAll();
+    //
+    //   //Assert
+    //   CollectionAssert.AreNotEqual(newList, resultList);
+    // }
+    //
+    // [TestMethod]
+    // public void DeleteAll_DeletesAllinviteesFromDatabase()
+    // {
+    //   //Arrange
+    //   string inviteeName = "Jane Doe";
+    //   string inviteeEmailAddress = "janedoe@mail.com";
+    //   invitee newinvitee = new invitee(inviteeName, inviteeEmailAddress);
+    //   newinvitee.Save();
+    //   invitee.DeleteAll();
+    //
+    //   //Act
+    //   List<invitee> newList = new List<invitee> { newinvitee };
+    //   List<invitee> resultList = invitee.GetAll();
+    //
+    //   //Assert
+    //   CollectionAssert.AreNotEqual(newList, resultList);
+    // }
+    //
+    // [TestMethod]
+    // public void Edit_UpdatesinviteeToDatabase()
+    // {
+    //   //Arrange
+    //   string inviteeName = "Jane Doe";
+    //   string inviteeEmailAddress = "janedoe@mail.com";
+    //   invitee newinvitee = new invitee(inviteeName, inviteeEmailAddress);
+    //   newinvitee.Save();
+    //
+    //   //Act
+    //   invitee foundinvitee = invitee.Find(newinvitee.GetId());
+    //   string newinviteeName = "John Smith";
+    //   string newinviteeEmailAddress = new string(2019, 05, 03);
+    //   foundinvitee.Edit(newinviteeName, newinviteeEmailAddress);
+    //   invitee updatedinvitee = invitee.Find(newinvitee.GetId());
+    //
+    //   List<invitee> result = invitee.GetAll();
+    //   List<invitee> testList = new List<invitee>{updatedinvitee};
+    //
+    //   //Assert
+    //   CollectionAssert.AreEqual(testList, result);
+    // }
+    //
+    // [TestMethod]
+    // public void Save_SavesEventinviteeToDatabase_EventList()
+    // {
+    //   //Arrange
+    //   string eventName = "July 4th BBQ";
+    //   string eventDate = "janedoe@mail.com";
+    //   string eventLocation = "Capitol Hill";
+    //   int menusId = 1;
+    //   Event newEvent = new Event(eventName, eventDate, eventLocation, menusId);
+    //   newEvent.Save();
+    //
+    //   string inviteeName = "Jane Doe";
+    //   string inviteeEmailAddress = "janedoe@mail.com";
+    //   invitee newinvitee = new invitee(inviteeName, inviteeEmailAddress);
+    //   newinvitee.Save();
+    //
+    //   //Act
+    //   Event foundEvent = Event.Find(newEvent.GetId());
+    //   invitee foundinvitee = invitee.Find(newinvitee.GetId());
+    //   foundinvitee.AddEvent(foundEvent);
+    //
+    //   List<Event> result = foundinvitee.GetEvents();
+    //   List<Event> testList = new List<Event>{foundEvent};
+    //
+    //   //Assert
+    //   CollectionAssert.AreEqual(testList, result);
+    // }
   }
 }
--- EventPlanner/Models/Invitee.cs
+++ EventPlanner/Models/Invitee.cs
@@ -6,6 +6,270 @@ namespace EventPlanner.Models
 {
   public class Invitee
   {
+    private string _inviteeName;
+    private string _inviteeEmailAddress;
+    private int _id;
 
+    public Invitee(string inviteeName, string inviteeEmailAddress, int id = 0)
+    {
+      _inviteeName = inviteeName;
+      _inviteeEmailAddress = inviteeEmailAddress;
+      _id = id;
+    }
+
+    // public string GetinviteeName()
+    // {
+    //   return _inviteeName;
+    // }
+    //
+    // public void SetinviteeName(string newinviteeName)
+    // {
+    //   _inviteeName = newinviteeName;
+    // }
+    //
+    // public string GetinviteeEmailAddress()
+    // {
+    //   return _inviteeEmailAddress;
+    // }
+    //
+    // public void GetinviteeEmailAddress_ReturnsinviteeEmailAddress_string(string newinviteeEmailAddress)
+    // {
+    //   _inviteeEmailAddress = newinviteeEmailAddress;
+    // }
+    //
+    // public void SetinviteeEmailAddress(string newinviteeEmailAddress)
+    // {
+    //   _inviteeEmailAddress = newinviteeEmailAddress;
+    // }
+    //
+    // public int GetId()
+    // {
+    //   return _id;
+    // }
+    //
+    // public static void ClearAll()
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"DELETE FROM events_invitees;
+    //                       DELETE FROM invitees;";
+    //   cmd.ExecuteNonQuery();
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    // }
+    //
+    // public void Save()
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"INSERT INTO invitees (invitee_name, invitee_email_address)
+    //                       VALUES (@inviteeName, @inviteeEmailAddress);";
+    //   MySqlParameter inviteeName = new MySqlParameter();
+    //   inviteeName.ParameterName = "@inviteeName";
+    //   inviteeName.Value = this._inviteeName;
+    //   cmd.Parameters.Add(inviteeName);
+    //   MySqlParameter inviteeEmailAddress = new MySqlParameter();
+    //   inviteeEmailAddress.ParameterName = "@inviteeEmailAddress";
+    //   inviteeEmailAddress.Value = this._inviteeEmailAddress;
+    //   cmd.Parameters.Add(inviteeEmailAddress);
+    //     cmd.ExecuteNonQuery();
+    //   _id = (int) cmd.LastInsertedId;
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    // }
+    //
+    // public static List<invitee> GetAll()
+    // {
+    //   List<invitee> allinvitees = new List<invitee> {};
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"SELECT * FROM invitees;";
+    //   var rdr = cmd.ExecuteReader() as MySqlDataReader;
+    //   while(rdr.Read())
+    //   {
+    //     int inviteeId = rdr.GetInt32(0);
+    //     string inviteeName = rdr.GetString(1);
+    //     string inviteeEmailAddress = rdr.Getstring(2);
+    //     invitee newinvitee = new invitee(inviteeName, inviteeEmailAddress, inviteeId);
+    //     allinvitees.Add(newinvitee);
+    //   }
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    //   return allinvitees;
+    // }
+    //
+    // public static invitee Find(int id)
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"SELECT * FROM invitees WHERE id = (@searchId);";
+    //   MySqlParameter searchId = new MySqlParameter();
+    //   searchId.ParameterName = "@searchId";
+    //   searchId.Value = id;
+    //   cmd.Parameters.Add(searchId);
+    //   var rdr = cmd.ExecuteReader() as MySqlDataReader;
+    //   int inviteeId = 0;
+    //   string inviteeName = "";
+    //   string inviteeEmailAddress = new string(1900, 1, 1);
+    //     while(rdr.Read())
+    //   {
+    //     inviteeId = rdr.GetInt32(0);
+    //     inviteeName = rdr.GetString(1);
+    //     inviteeEmailAddress = rdr.Getstring(2);
+    //     }
+    //   invitee newinvitee = new invitee(inviteeName, inviteeEmailAddress, inviteeId);
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    //     return newinvitee;
+    // }
+    //
+    // public void Delete()
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"DELETE FROM events_invitees WHERE invitees_id = (@searchid);
+    //                       DELETE FROM invitees WHERE id = (@searchId);";
+    //   MySqlParameter searchId = new MySqlParameter();
+    //   searchId.ParameterName = "@searchId";
+    //   searchId.Value = this._id;
+    //   cmd.Parameters.Add(searchId);
+    //   cmd.ExecuteNonQuery();
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    // }
+    //
+    // public static void DeleteAll()
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"DELETE FROM events_invitees;
+    //                       DELETE FROM invitees";
+    //   cmd.ExecuteNonQuery();
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    // }
+    //
+    // public void Edit(string newinviteeName, string newinviteeEmailAddress)
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"UPDATE invitees SET invitee_name = (@inviteeName), invitee_email_address = (@inviteeEmailAddress)
+    //                        WHERE id = (@inviteeId);";
+    //   MySqlParameter inviteeNameParameter = new MySqlParameter();
+    //   inviteeNameParameter.ParameterName = "@inviteeName";
+    //   inviteeNameParameter.Value = newinviteeName;
+    //   cmd.Parameters.Add(inviteeNameParameter);
+    //   MySqlParameter inviteeEmailAddressParameter = new MySqlParameter();
+    //   inviteeEmailAddressParameter.ParameterName = "@inviteeEmailAddress";
+    //   inviteeEmailAddressParameter.Value = newinviteeEmailAddress;
+    //   cmd.Parameters.Add(inviteeEmailAddressParameter);
+    //   MySqlParameter inviteeIdParameter = new MySqlParameter();
+    //   inviteeIdParameter.ParameterName = "@inviteeId";
+    //   inviteeIdParameter.Value = this._id;
+    //   cmd.Parameters.Add(inviteeIdParameter);
+    //   cmd.ExecuteNonQuery();
+    //   _inviteeName = newinviteeName;
+    //   _inviteeEmailAddress = newinviteeEmailAddress;
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    // }
+    //
+    // public void AddEvent(Event newEvent)
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"INSERT INTO events_invitees (events_id, invitees_id) VALUES (@eventId, @inviteeId);";
+    //   MySqlParameter eventIdParameter = new MySqlParameter();
+    //   eventIdParameter.ParameterName = "@eventId";
+    //   eventIdParameter.Value = newEvent.GetId();
+    //   cmd.Parameters.Add(eventIdParameter);
+    //   MySqlParameter inviteeIdParameter = new MySqlParameter();
+    //   inviteeIdParameter.ParameterName = "@inviteeId";
+    //   inviteeIdParameter.Value = this._id;
+    //   cmd.Parameters.Add(inviteeIdParameter);
+    //   cmd.ExecuteNonQuery();
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    // }
+    //
+    // public List<Event> GetEvents()
+    // {
+    //   List<Event> allEventinvitees = new List<Event> {};
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"SELECT *
+    //                         FROM events
+    //                        WHERE id IN (SELECT events_id FROM events_invitees WHERE invitees_id = @inviteeId);";
+    //   MySqlParameter inviteeId = new MySqlParameter();
+    //   inviteeId.ParameterName = "@inviteeId";
+    //   inviteeId.Value = this._id;
+    //   cmd.Parameters.Add(inviteeId);
+    //   var rdr = cmd.ExecuteReader() as MySqlDataReader;
+    //   while(rdr.Read())
+    //   {
+    //     int eventId = rdr.GetInt32(0);
+    //     string eventName = rdr.GetString(1);
+    //     string eventDate = rdr.Getstring(2);
+    //     string eventLocation = rdr.GetString(3);
+    //     int menusId = rdr.GetInt32(4);
+    //     Event newEvent = new Event(eventName, eventDate, eventLocation, menusId, eventId);
+    //     allEventinvitees.Add(newEvent);
+    //   }
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    //   return allEventinvitees;
+    // }
+    //
+    // public override bool Equals(System.Object otherinvitee)
+    // {
+    //   if (!(otherinvitee is invitee))
+    //   {
+    //     return false;
+    //   }
+    //   else
+    //   {
+    //     invitee newinvitee = (invitee) otherinvitee;
+    //     bool idEquality = this.GetId().Equals(newinvitee.GetId());
+    //     bool inviteeNameEquality = this.GetinviteeName().Equals(newinvitee.GetinviteeName());
+    //     bool inviteeEmailAddressEquality = this.GetinviteeEmailAddress().Equals(newinvitee.GetinviteeEmailAddress());
+    //     return (idEquality && inviteeNameEquality && inviteeEmailAddressEquality);
+    //   }
+    // }
   }
 }
