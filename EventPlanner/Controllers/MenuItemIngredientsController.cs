ca9dd0cec28f56353348eed6de954d3a17a02a74
Guest User <Guest@epicodus-50.local>
2019-03-12 10:43:55 -0700

    fix menuscontroller

--- EventPlanner/Controllers/MenuItemIngredientsController.cs
+++ EventPlanner/Controllers/MenuItemIngredientsController.cs
@@ -1,85 +1,85 @@
-// using System.Collections.Generic;
-// using System;
-// using Microsoft.AspNetCore.Mvc;
-// using EventPlanner.Models;
-//
-// namespace EventPlanner.Controllers
-// {
-//   public class MenuIitemIngredientsController : Controller
-//   {
-//     [HttpGet("/menuItemIngredients")]
-//     public ActionResult Index()
-//     {
-//       List<MenuItemIngredient> allMenuItemIngredients = MenuItemIngredient.GetAll();
-//       return View(allMenuItemIngredients);
-//     }
-//
-//     [HttpGet("/menuItemIngredients/new")]
-//     public ActionResult New()
-//     {
-//       return View();
-//     }
-//     [HttpPost("/menuItemIngredients/new")]
-//     public ActionResult Create(string ingredientDescription, int menuItemId, int storeId)
-//     {
-//       MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(ingredientDescription, menuItemId, storeId);
-//       newMenuItemIngredient.Save();
-//       return RedirectToAction("Index");
-//     }
-//
-//     [HttpGet("/menuItemIngredients/{menuItemIngredientId}")]
-//     public ActionResult Show(int menuItemIngredientId)
-//     {
-//       MenuItemIngredient menuItemIngredient = MenuItemIngredient.Find(menuItemIngredientId);
-//       MenuItemIngredient menuItems = MenuItemIngredient.GetMenuItem(menuItemIngredientId);
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       model.Add("menuItemIngredient", menuItemIngredient);
-//       model.Add("menuItems", menuItems);
-//       return View(model);
-//     }
-//
-//     [HttpPost("/menuItemIngredients/{menuItemIngredientId}/items/new")]
-//     public ActionResult AddMenuItem(int menuItemIngredientId, int menuItemId)
-//     {
-//       MenuItemIngredient menuItemIngredient = MenuItemIngredient.Find(menuItemIngredientId);
-//       menuItemIngredient.AddMenuItem(Menu.Find(menuItemId));
-//       return RedirectToAction("Show");
-//     }
-//     [HttpPost("/menuItemIngredients/{menuItemIngredientId}/items/new")]
-//     public ActionResult DeleteMenuItem(int menuItemIngredientId, int menuItemId)
-//     {
-//       MenuItemIngredient menuItemIngredient = MenuItemIngredient.Find(menuItemIngredientId);
-//       menuItemIngredient.DeleteMenuItem(Menu.Find(menuItemId));
-//       return RedirectToAction("Show");
-//     }
-//
-//     [HttpPost("/menuItemIngredients/{menuItemIngredientId}/delete")]
-//     public ActionResult Delete(int menuItemIngredientId)
-//     {
-//       MenuItemIngredient.Delete(menuItemIngredientId);
-//       return RedirectToAction("Index");
-//     }
-//     [HttpPost("/menuItemIngredients/delete")]
-//     public ActionResult DeleteAll()
-//     {
-//       MenuItemIngredient.DeleteAll();
-//       return RedirectToAction("Index");
-//     }
-//
-//     [HttpGet("/menuItemIngredients/{menuItemIngredientId}/edit")]
-//     public ActionResult Edit(int menuItemIngredientId)
-//     {
-//       MenuItemIngredient menuItemIngredient = MenuItemIngredient.Find(menuItemIngredientId);
-//       Dictionary<string, object) model = new Dictionary<string, object>();
-//       model.Add("menuItemIngredient", menuItemIngredient);
-//       return View(model);
-//     }
-//     [HttpPost("/menuItemIngredients/{menuItemIngredientId}/edit")]
-//     public ActionResult Update(int menuItemIngredientId, string ingredientDescription, int menuItemId, int storeId)
-//     {
-//       MenuItemIngredient menuItemIngredient = Menu.Find(menuItemIngredientId);
-//       menuItemIngredient.Edit(menuItemIngredientId, ingredientDescription, menuItemId, storeId);
-//       return RedirectToAction("Index");
-//     }
-//   }
-// }
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using EventPlanner.Models;
+
+namespace EventPlanner.Controllers
+{
+  // public class MenuIitemIngredientsController : Controller
+  // {
+  //   [HttpGet("/menuItemIngredients")]
+  //   public ActionResult Index()
+  //   {
+  //     List<MenuItemIngredient> allMenuItemIngredients = MenuItemIngredient.GetAll();
+  //     return View(allMenuItemIngredients);
+  //   }
+  //
+  //   [HttpGet("/menuItemIngredients/new")]
+  //   public ActionResult New()
+  //   {
+  //     return View();
+  //   }
+  //   [HttpPost("/menuItemIngredients/new")]
+  //   public ActionResult Create(string ingredientDescription, int menuItemId, int storeId)
+  //   {
+  //     MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(ingredientDescription, menuItemId, storeId);
+  //     newMenuItemIngredient.Save();
+  //     return RedirectToAction("Index");
+  //   }
+  //
+  //   [HttpGet("/menuItemIngredients/{menuItemIngredientId}")]
+  //   public ActionResult Show(int menuItemIngredientId)
+  //   {
+  //     MenuItemIngredient menuItemIngredient = MenuItemIngredient.Find(menuItemIngredientId);
+  //     MenuItemIngredient menuItems = MenuItemIngredient.GetMenuItem(menuItemIngredientId);
+  //     Dictionary<string, object> model = new Dictionary<string, object>();
+  //     model.Add("menuItemIngredient", menuItemIngredient);
+  //     model.Add("menuItems", menuItems);
+  //     return View(model);
+  //   }
+  //
+  //   [HttpPost("/menuItemIngredients/{menuItemIngredientId}/items/new")]
+  //   public ActionResult AddMenuItem(int menuItemIngredientId, int menuItemId)
+  //   {
+  //     MenuItemIngredient menuItemIngredient = MenuItemIngredient.Find(menuItemIngredientId);
+  //     menuItemIngredient.AddMenuItem(Menu.Find(menuItemId));
+  //     return RedirectToAction("Show");
+  //   }
+  //   [HttpPost("/menuItemIngredients/{menuItemIngredientId}/items/new")]
+  //   public ActionResult DeleteMenuItem(int menuItemIngredientId, int menuItemId)
+  //   {
+  //     MenuItemIngredient menuItemIngredient = MenuItemIngredient.Find(menuItemIngredientId);
+  //     menuItemIngredient.DeleteMenuItem(Menu.Find(menuItemId));
+  //     return RedirectToAction("Show");
+  //   }
+  //
+  //   [HttpPost("/menuItemIngredients/{menuItemIngredientId}/delete")]
+  //   public ActionResult Delete(int menuItemIngredientId)
+  //   {
+  //     MenuItemIngredient.Delete(menuItemIngredientId);
+  //     return RedirectToAction("Index");
+  //   }
+  //   [HttpPost("/menuItemIngredients/delete")]
+  //   public ActionResult DeleteAll()
+  //   {
+  //     MenuItemIngredient.DeleteAll();
+  //     return RedirectToAction("Index");
+  //   }
+  //
+  //   [HttpGet("/menuItemIngredients/{menuItemIngredientId}/edit")]
+  //   public ActionResult Edit(int menuItemIngredientId)
+  //   {
+  //     MenuItemIngredient menuItemIngredient = MenuItemIngredient.Find(menuItemIngredientId);
+  //     Dictionary<string, object> model = new Dictionary<string, object>();
+  //     model.Add("menuItemIngredient", menuItemIngredient);
+  //     return View(model);
+  //   }
+  //   [HttpPost("/menuItemIngredients/{menuItemIngredientId}/edit")]
+  //   public ActionResult Update(int menuItemIngredientId, string ingredientDescription, int menuItemId, int storeId)
+  //   {
+  //     MenuItemIngredient menuItemIngredient = Menu.Find(menuItemIngredientId);
+  //     menuItemIngredient.Edit(menuItemIngredientId, ingredientDescription, menuItemId, storeId);
+  //     return RedirectToAction("Index");
+  //   }
+  // }
+}
--- EventPlanner/Controllers/MenusController.cs
+++ EventPlanner/Controllers/MenusController.cs
@@ -1,85 +1,86 @@
-// using System.Collections.Generic;
-// using System;
-// using Microsoft.AspNetCore.Mvc;
-// using MenuPlanner.Models;
-//
-// namespace MenuPlanner.Controllers
-// {
-//   public class MenusController : Controller
-//   {
-//     [HttpGet("/menus")]
-//     public ActionResult Index()
-//     {
-//       List<Menu> allMenus = Menu.GetAll();
-//       return View(allMenus);
-//     }
-//
-//     [HttpGet("/menus/new")]
-//     public ActionResult New()
-//     {
-//       return View();
-//     }
-//     [HttpPost("/menus/new")]
-//     public ActionResult Create(string menuTheme)
-//     {
-//       Menu newMenu = new Menu(menuTheme);
-//       newMenu.Save();
-//       return RedirectToAction("Index");
-//     }
-//
-//     [HttpGet("/menus/{menuId}")]
-//     public ActionResult Show(int menuId)
-//     {
-//       Menu menu = Menu.Find(menuId);
-//       Menu menuItems = Menu.GetMenuItems(menuId);
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       model.Add("menu", menu);
-//       model.Add("menuItems", menuItems);
-//       return View(model);
-//     }
-//
-//     [HttpPost("/menus/{menuId}/menuItem/new")]
-//     public ActionResult AddMenuItem(int menuId, int menuItemId)
-//     {
-//       Menu menu = Menu.Find(menuId);
-//       menu.AddMenuItem(MenuItem.Find(menuItemId));
-//       return RedirectToAction("Show");
-//     }
-//     [HttpPost("/menus/{menuId}/menuItem/delete")]
-//     public ActionResult DeleteMenuItem(int menuId, int menuItemId)
-//     {
-//       Menu menu = Menu.Find(menuId);
-//       menu.DeleteMenuItem(MenuItem.Find(menuItemId));
-//       return RedirectToAction("Show");
-//     }
-//
-//     [HttpPost("/menus/{menuId}/delete")]
-//     public ActionResult Delete(int menuId)
-//     {
-//       Menu.Delete(menuId);
-//       return RedirectToAction("Index");
-//     }
-//     [HttpPost("/menus/delete")]
-//     public ActionResult DeleteAll()
-//     {
-//       Menu.DeleteAll();
-//       return RedirectToAction("Index");
-//     }
-//
-//     [HttpGet("/menus/{menuId}/edit")]
-//     public ActionResult Edit(int menuId)
-//     {
-//       Menu menu = Menu.Find(menuId);
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       model.Add("Menu", menu);
-//       return View(model);
-//     }
-//     [HttpPost("/menus/{menuId}/edit")]
-//     public ActionResult Update(int menuId, string menuTheme)
-//     {
-//       Menu menu = Menu.Find(menuId);
-//       menu.Edit(menuId, menuTheme);
-//       return RedirectToAction("Index");
-//     }
-//   }
-// }
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using EventPlanner.Models;
+
+namespace MenuPlanner.Controllers
+{
+  public class MenusController : Controller
+  {
+    [HttpGet("/menus")]
+    public ActionResult Index()
+    {
+      List<Menu> allMenus = Menu.GetAll();
+      return View(allMenus);
+    }
+
+    [HttpGet("/menus/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/menus/new")]
+    public ActionResult Create(string menuTheme)
+    {
+      Menu newMenu = new Menu(menuTheme);
+      newMenu.Save();
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/menus/{menuId}")]
+    public ActionResult Show(int menuId)
+    {
+      Menu menu = Menu.Find(menuId);
+      // List<MenuItem> menuItems = Menu.GetMenuItems(menuId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("menu", menu);
+      // model.Add("menuItems", menuItems);
+      return View(model);
+    }
+
+    // [HttpPost("/menus/{menuId}/menuItem/new")]
+    // public ActionResult AddMenuItem(int menuId, int menuItemId)
+    // {
+    //   Menu menu = Menu.Find(menuId);
+    //   menu.AddMenuItem(MenuItem.Find(menuItemId));
+    //   return RedirectToAction("Show");
+    // }
+    // [HttpPost("/menus/{menuId}/menuItem/delete")]
+    // public ActionResult DeleteMenuItem(int menuId, int menuItemId)
+    // {
+    //   Menu menu = Menu.Find(menuId);
+    //   menu.DeleteMenuItem(MenuItem.Find(menuItemId));
+    //   return RedirectToAction("Show");
+    // }
+
+    [HttpPost("/menus/{menuId}/delete")]
+    public ActionResult Delete(int menuId)
+    {
+      Menu menu = Menu.Find(menuId);
+      menu.Delete();
+      return RedirectToAction("Index");
+    }
+    [HttpPost("/menus/delete")]
+    public ActionResult DeleteAll()
+    {
+      Menu.DeleteAll();
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/menus/{menuId}/edit")]
+    public ActionResult Edit(int menuId)
+    {
+      Menu menu = Menu.Find(menuId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("Menu", menu);
+      return View(model);
+    }
+    [HttpPost("/menus/{menuId}/edit")]
+    public ActionResult Update(int menuId, string menuTheme)
+    {
+      Menu menu = Menu.Find(menuId);
+      menu.Edit(menuTheme);
+      return RedirectToAction("Index");
+    }
+  }
+}
