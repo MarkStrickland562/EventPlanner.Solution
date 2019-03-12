f3e0af19bcef1bf4ad72a96b7fb4c78d92cd4e92
Guest User <Guest@epicodus-50.local>
2019-03-12 10:31:18 -0700

    comment out controllers, fix event controller

--- EventPlanner.Tests/ControllerTests/EventsControllerTests.cs
+++ EventPlanner.Tests/ControllerTests/EventsControllerTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using EventPlanner.Controllers;
 using EventPlanner.Models;
@@ -7,48 +8,57 @@ using EventPlanner.Models;
 namespace EventPlanner.Tests
 {
   [TestClass]
-  public class EventsControllerTest //: IDisposable
+  public class EventsControllerTest : IDisposable
   {
     public void Dispose()
     {
-      // Events.ClearAll();
+      Event.ClearAll();
+    }
+
+    [TestMethod]
+    public void Index_ReturnsCorrectView_True()
+    {
+      EventsController controller = new EventsController();
+      Assert.IsInstanceOfType(controller.Index(), typeof(ViewResult));
+    }
+    [TestMethod]
+    public void Index_HasCorrectModelType_EventsList()
+    {
+      ViewResult indexView = new EventsController().Index() as ViewResult;
+      var result = indexView.ViewData.Model;
+      Assert.IsInstanceOfType(result, typeof(List<Event>));
+    }
+
+    [TestMethod]
+    public void New_ReturnCorrectViewOfForm_True()
+    {
+      ActionResult newView = new EventsController().New();
+      Assert.IsInstanceOfType(newView, typeof(ViewResult));
+    }
+    [TestMethod]
+    public void Create_CreatesNewInstanceOfStylist_True()
+    {
+      ActionResult createPost = new EventsController().Create("TestName", (new DateTime(2019, 12, 31, 21, 30, 0, DateTimeKind.Utc)), "TestLocation", 1);
+      Assert.IsInstanceOfType(createPost, typeof(ActionResult));
     }
 
     // [TestMethod]
-    // public void Index_ReturnsCorrectView_True()
+    // public void Show_ProcessesShowModelCorrectly_True()
     // {
-    //   EventsController controller = new EventsController();
-    //   Assert.IsInstanceOfType(controller.Index(), typeof(ViewResult));
-    // }
-    // [TestMethod]
-    // public void Index_HasCorrectModelType_EventsList()
-    // {
-    //   ViewResult indexView = new EventsController().Index() as ViewResult;
-    //   var result = indexView.ViewData.Model;
-    //   Assert.IsInstanceOfType(result, typeof(List<Events>));
-    // }
-    //
-    // [TestMethod]
-    // public void New_ReturnCorrectViewOfForm_True()
-    // {
-    //   ActionResult newView = new EventsController().New();
-    //   Assert.IsInstanceOfType(newView, typeof(ViewResult));
-    // }
-    // [TestMethod]
-    // public void Create_CreatesNewInstanceOfStylist_True()
-    // {
-    //   ActionResult createPost = new EventsController().Create("TestName", "12/31/2019 9:30:00 PM", "TestLocation", 1);
-    //   Assert.IsInstanceOfType(createPost, typeof(ActionResult));
-    // }
-    //
-    // [TestMethod]
-    // public void Delete_DeletesEventObject_True()
-    // {
-    //   Event newEvent = new Event("TestName", "12/31/2019 9:30:00 PM", "TestLocation", 1);
+    //   Event newEvent = new Event("TestName", (new DateTime(2019, 12, 31, 21, 30, 0, DateTimeKind.Utc)), "TestLocation", 1);
     //   newEvent.Save();
-    //   ActionResult deletePost = new EventsController().Delete(newEvent.GetId());
-    //   Assert.IsInstanceOfType(deletePost, typeof(ActionResult));
-    //   CollectionAssert.AreEqual(new List<Events> { }, Event.GetAll());
+    //   ActionResult show = new EventsController().Show(newEvent.GetId());
+    //   Assert.AreEqual(3, show.Count);
     // }
+
+    [TestMethod]
+    public void Delete_DeletesEventObject_True()
+    {
+      Event newEvent = new Event("TestName", (new DateTime(2019, 12, 31, 21, 30, 0, DateTimeKind.Utc)), "TestLocation", 1);
+      newEvent.Save();
+      ActionResult deletePost = new EventsController().Delete(newEvent.GetId());
+      Assert.IsInstanceOfType(deletePost, typeof(ActionResult));
+      CollectionAssert.AreEqual(new List<Event> { }, Event.GetAll());
+    }
   }
 }
