e2b61ee42df76ef34b8a4db6849bd6a423328696
Mark Strickland <mstrickland@globys.com>
2019-03-12 10:46:13 -0700

    Add MenuItemIngredient Constructor() method and test.

--- EventPlanner.Tests/ModelTests/MenuItemIngredientTests.cs
+++ EventPlanner.Tests/ModelTests/MenuItemIngredientTests.cs
@@ -15,9 +15,261 @@ namespace EventPlanner.Tests
 
     public void Dispose()
     {
-//      Event.ClearAll();
+//      MenuItemIngredient.ClearAll();
     }
 
+    [TestMethod]
+    public void menuItemIngredientConstructor_CreatesInstanceOfMenuItemIngredient_MenuItemIngredient()
+    {
+      //Arrange, Act
+      string menuItemIngredientDescription = "Avocadoes";
+      int menuItemsId = 1;
+      int storeId = 1;
+      MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(menuItemIngredientDescription, menuItemsId, storeId);
 
+      //Assert
+      Assert.AreEqual(typeof(MenuItemIngredient), newMenuItemIngredient.GetType());
+    }
+
+    // [TestMethod]
+    // public void GetMenuItemIngredientDescription_ReturnsMenuItemIngredientDescription_String()
+    // {
+    //   //Arrange
+    //   string menuItemIngredientDescription = "Avocadoes";
+    //   int menuItemsId = 1;
+    //   int storeId = 1;
+    //   MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(menuItemIngredientDescription, menuItemsId, storeId);
+    //
+    //   //Act
+    //   string result = newMenuItemIngredient.GetMenuItemIngredientDescription();
+    //
+    //   //Assert
+    //   Assert.AreEqual(menuItemIngredientDescription, result);
+    // }
+    //
+    // [TestMethod]
+    // public void SetMenuItemIngredientDescription_SetMenuItemIngredientDescription_String()
+    // {
+    //   //Arrange
+    //   string menuItemIngredientDescription = "Avocadoes";
+    //   int menuItemsId = 1;
+    //   int storeId = 1;
+    //   MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(menuItemIngredientDescription, menuItemsId, storeId);
+    //
+    //   //Act
+    //   string updatedMenuItemIngredientDescription = "Tomatoes";
+    //   newMenuItemIngredient.SetMenuItemIngredientDescription(updatedMenuItemIngredientDescription);
+    //   string result = newMenuItemIngredient.GetMenuItemIngredientDescription();
+    //
+    //   //Assert
+    //   Assert.AreEqual(updatedMenuItemIngredientDescription, result);
+    // }
+    //
+    // [TestMethod]
+    // public void GetMenuItemsId_ReturnsMenuItemsId_Int()
+    // {
+    //   //Arrange
+    //   string menuItemIngredientDescription = "Avocadoes";
+    //   int menuItemsId = 1;
+    //   int storeId = 1;
+    //   MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(menuItemIngredientDescription, menuItemsId, storeId);
+    //
+    //   //Act
+    //   string result = newMenuItemIngredient.GetMenuItemsId();
+    //
+    //   //Assert
+    //   Assert.AreEqual(menuItemsId, result);
+    // }
+    //
+    // [TestMethod]
+    // public void SetMenuItemsId_SetMenuItemsId_Int()
+    // {
+    //   //Arrange
+    //   string menuItemIngredientDescription = "Avocadoes";
+    //   int menuItemsId = 1;
+    //   int storeId = 1;
+    //   MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(menuItemIngredientDescription, menuItemsId, storeId);
+    //
+    //   //Act
+    //   int updatedMenuItemsId = 2;
+    //   newMenuItemIngredient.SetMenuItemsId(updatedMenuItemsId);
+    //   string result = newMenuItemIngredient.GetMenuItemsId();
+    //
+    //   //Assert
+    //   Assert.AreEqual(updatedMenuItemsId, result);
+    // }
+    //
+    // [TestMethod]
+    // public void GetStoreId_ReturnsStoreId_Int()
+    // {
+    //   //Arrange
+    //   string menuItemIngredientDescription = "Avocadoes";
+    //   int menuItemsId = 1;
+    //   int storeId = 1;
+    //   MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(menuItemIngredientDescription, menuItemsId, storeId);
+    //
+    //   //Act
+    //   string result = newMenuItemIngredient.GetStoreId();
+    //
+    //   //Assert
+    //   Assert.AreEqual(storeId, result);
+    // }
+    //
+    // [TestMethod]
+    // public void SetStoreId_SetStoreId_Int()
+    // {
+    //   //Arrange
+    //   string menuItemIngredientDescription = "Avocadoes";
+    //   int menuItemsId = 1;
+    //   int storeId = 1;
+    //   MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(menuItemIngredientDescription, menuItemsId, storeId);
+    //
+    //   //Act
+    //   int updatedStoreId = 2;
+    //   newMenuItemIngredient.SetStoreId(updatedStoreId);
+    //   string result = newMenuItemIngredient.GetStoreId();
+    //
+    //   //Assert
+    //   Assert.AreEqual(updatedStoreId, result);
+    // }
+    //
+    // [TestMethod]
+    // public void GetId_ReturnsMenuItemIngredientId_Int()
+    // {
+    //   //Arrange
+    //   string menuItemIngredientDescription = "Avocadoes";
+    //   int menuItemsId = 1;
+    //   int storeId = 1;
+    //   MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(menuItemIngredientDescription, menuItemsId, storeId);
+    //
+    //   //Act
+    //   int result = newMenuItemIngredient.GetId();
+    //
+    //   //Assert
+    //   Assert.AreEqual(0, result);
+    // }
+    //
+    // [TestMethod]
+    // public void Save_SavesMenuItemIngredientToDatabase_MenuItemIngredientList()
+    // {
+    //   //Arrange
+    //   string menuItemIngredientDescription = "Avocadoes";
+    //   int menuItemsId = 1;
+    //   int storeId = 1;
+    //   MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(menuItemIngredientDescription, menuItemsId, storeId);
+    //   newMenuItemIngredient.Save();
+    //
+    //   //Act
+    //   List<MenuItemIngredient> result = MenuItemIngredient.GetAll();
+    //   List<MenuItemIngredient> testList = new List<MenuItemIngredient>{newMenuItemIngredient};
+    //
+    //   //Assert
+    //   CollectionAssert.AreEqual(testList, result);
+    // }
+    //
+    // [TestMethod]
+    // public void GetAll_ReturnsAllMenuItemIngredientObjects_MenuItemIngredientList()
+    // {
+    //   //Arrange
+    //   string menuItemIngredientDescription1 = "Avocadoes";
+    //   int menuItemsId1 = 1;
+    //   int storeId1 = 1;
+    //   MenuItemIngredient newMenuItemIngredient1 = new MenuItemIngredient(menuItemIngredientDescription1, menuItemsId1, storeId1);
+    //   newMenuItemIngredient1.Save();
+    //
+    //   string menuItemIngredientDescription2 = "Tomatoes";
+    //   int menuItemsId2 = 2;
+    //   int storeId2 = 2;
+    //   MenuItemIngredient newMenuItemIngredient2 = new MenuItemIngredient(menuItemIngredientDescription2, menuItemsId2, storeId2);
+    //   newMenuItemIngredient2.Save();
+    //
+    //   List<MenuItemIngredient> newList = new List<MenuItemIngredient> { newMenuItemIngredient1, newMenuItemIngredient2};
+    //
+    //   //Act
+    //   List<MenuItemIngredient> result = MenuItemIngredient.GetAll();
+    //
+    //   //Assert
+    //   CollectionAssert.AreEqual(newList, result);
+    // }
+    //
+    // [TestMethod]
+    // public void Find_ReturnsMenuItemIngredientInDatabase_MenuItemIngredient()
+    // {
+    //   //Arrange
+    //   string menuItemIngredientDescription = "Avocadoes";
+    //   int menuItemsId = 1;
+    //   int storeId = 1;
+    //   MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(menuItemIngredientDescription, menuItemsId, storeId);
+    //   newMenuItemIngredient.Save();
+    //
+    //   //Act
+    //   MenuItemIngredient foundMenuItemIngredient = MenuItemIngredient.Find(newMenuItemIngredient.GetId());
+    //
+    //   //Assert
+    //   Assert.AreEqual(newMenuItemIngredient, foundMenuItemIngredient);
+    // }
+    //
+    // [TestMethod]
+    // public void Delete_DeletesMenuItemIngredientFromDatabase()
+    // {
+    //   //Arrange
+    //   string menuItemIngredientDescription = "Avocadoes";
+    //   int menuItemsId = 1;
+    //   int storeId = 1;
+    //   MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(menuItemIngredientDescription, menuItemsId, storeId);
+    //   newMenuItemIngredient.Save();
+    //   newMenuItemIngredient.Delete();
+    //
+    //   //Act
+    //   List<MenuItemIngredient> newList = new List<MenuItemIngredient> { newMenuItemIngredient };
+    //   List<MenuItemIngredient> resultList = MenuItemIngredient.GetAll();
+    //
+    //   //Assert
+    //   CollectionAssert.AreNotEqual(newList, resultList);
+    // }
+    //
+    // [TestMethod]
+    // public void DeleteAll_DeletesAllMenuItemIngredientsFromDatabase()
+    // {
+    //   //Arrange
+    //   string menuItemIngredientDescription = "Avocadoes";
+    //   int menuItemsId = 1;
+    //   int storeId= 1;
+    //   MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(menuItemIngredientDescription, menuItemsId, storeId);
+    //   newMenuItemIngredient.Save();
+    //   MenuItemIngredient.DeleteAll();
+    //
+    //   //Act
+    //   List<MenuItemIngredient> newList = new List<MenuItemIngredient> { newMenuItemIngredient };
+    //   List<MenuItemIngredient> resultList = MenuItemIngredient.GetAll();
+    //
+    //   //Assert
+    //   CollectionAssert.AreNotEqual(newList, resultList);
+    // }
+    //
+    // [TestMethod]
+    // public void Edit_UpdatesMenuItemIngredientToDatabase()
+    // {
+    //   //Arrange
+    //   string menuItemIngredientDescription = "Avocadoes";
+    //   int menuItemsId = 1;
+    //   int storeId = 1;
+    //   MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(menuItemIngredientDescription, menuItemsId, storeId);
+    //   newMenuItemIngredient.Save();
+    //
+    //   //Act
+    //   MenuItemIngredient foundMenuItemIngredient = MenuItemIngredient.Find(newMenuItemIngredient.GetId());
+    //   string newMenuItemIngredientDescription = "Tomatoes";
+    //   int newMenuItemsId = 2;
+    //   int newStoreId = 2;
+    //   foundMenuItemIngredient.Edit(newMenuItemIngredientDescription, newMenuItemsId, newStoreId);
+    //   MenuItemIngredient updatedMenuItemIngredient = MenuItemIngredient.Find(newMenuItemIngredient.GetId());
+    //
+    //   List<MenuItemIngredient> result = MenuItemIngredient.GetAll();
+    //   List<MenuItemIngredient> testList = new List<MenuItemIngredient>{updatedMenuItemIngredient};
+    //
+    //   //Assert
+    //   CollectionAssert.AreEqual(testList, result);
+    // }
   }
 }
--- EventPlanner/Models/MenuItemIngredient.cs
+++ EventPlanner/Models/MenuItemIngredient.cs
@@ -6,6 +6,236 @@ namespace EventPlanner.Models
 {
   public class MenuItemIngredient
   {
+    private string _ingredientDescription;
+    private int _menuItemsId;
+    private int _storeId;
+    private int _id;
 
+    public MenuItemIngredient(string ingredientDescription, int menuItemsId, int storeId, int id = 0)
+    {
+      _ingredientDescription = ingredientDescription;
+      _menuItemsId = menuItemsId;
+      _storeId = storeId;
+      _id = id;
+    }
+
+    // public string GetIngredientDescription()
+    // {
+    //   return _ingredientDescription;
+    // }
+    //
+    // public void SetIngredientDescription(string newIngredientDescription)
+    // {
+    //   _ingredientDescription = newIngredientDescription;
+    // }
+    //
+    // public string GetMenuItemsId()
+    // {
+    //   return _menuItemsId;
+    // }
+    //
+    // public void SetMenuItemsId(string newMenuItemsId)
+    // {
+    //   _menuItemsId = newMenuItemsId;
+    // }
+    //
+    // public string GetStoreId()
+    // {
+    //   return _storeId;
+    // }
+    //
+    // public void SetStoreId(string newStoreId)
+    // {
+    //   _storeId = newStoreId;
+    // }
+    //
+    // public int GetId()
+    // {
+    //   return _id;
+    // }
+    //
+    // public static void ClearAll()
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"DELETE FROM menu_item_ingredients;";
+    //   cmd.ExecuteNonQuery();
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    // }
+    //
+    // public void Save()
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"INSERT INTO menu_item_ingredients (ingredient_description, menu_items_id, store_id)
+    //                       VALUES (@ingredientDescription, @menuItemsId, @storeId);";
+    //   MySqlParameter ingredientDescription = new MySqlParameter();
+    //   ingredientDescription.ParameterName = "@ingredientDescription";
+    //   ingredientDescription.Value = this._ingredientDescription;
+    //   cmd.Parameters.Add(ingredientDescription);
+    //   MySqlParameter menuItemsId = new MySqlParameter();
+    //   menuItemsId.ParameterName = "@menuItemsId";
+    //   menuItemsId.Value = this._menuItemsId;
+    //   cmd.Parameters.Add(menuItemsId);
+    //   MySqlParameter storeId = new MySqlParameter();
+    //   storeId.ParameterName = "@storeId";
+    //   storeId.Value = this._storeId;
+    //   cmd.Parameters.Add(storeId);
+    //   cmd.ExecuteNonQuery();
+    //   _id = (int) cmd.LastInsertedId;
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    // }
+    //
+    // public static List<MenuItemIngredient> GetAll()
+    // {
+    //   List<MenuItemIngredient> allMenuItemIngredients = new List<MenuItemIngredient> {};
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"SELECT * FROM menu_item_ingredients;";
+    //   var rdr = cmd.ExecuteReader() as MySqlDataReader;
+    //   while(rdr.Read())
+    //   {
+    //     int menuItemIngredientId = rdr.GetInt32(0);
+    //     string ingredientDescription = rdr.GetString(1);
+    //     int menuItemsId = rdr.GetInt32(2);
+    //     int storeId = rdr.GetInt32(3);
+    //     MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(ingredientDescription, menuItemsId, storeId, menuItemIngredientId);
+    //     allMenuItemIngredients.Add(newMenuItemIngredient);
+    //   }
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    //   return allMenuItemIngredients;
+    // }
+    //
+    // public static MenuItemIngredient Find(int id)
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"SELECT * FROM menu_item_ingredients WHERE id = (@searchId);";
+    //   MySqlParameter searchId = new MySqlParameter();
+    //   searchId.ParameterName = "@searchId";
+    //   searchId.Value = id;
+    //   cmd.Parameters.Add(searchId);
+    //   var rdr = cmd.ExecuteReader() as MySqlDataReader;
+    //   int menuItemIngredientId = 0;
+    //   string ingredientDescription = "";
+    //   int menuItemsId = 0;
+    //   int storeId = 0;
+    //   while(rdr.Read())
+    //   {
+    //     menuItemIngredientId = rdr.GetInt32(0);
+    //     ingredientDescription = rdr.GetString(1);
+    //     menuItemsId = rdr.GetInt32(2);
+    //     storeId = rdr.GetInt32();
+    //   }
+    //   MenuItemIngredient newMenuItemIngredient = new MenuItemIngredient(ingredientDescription, menuItemsId, storeId, menuItemIngredientId);
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    //     return newMenuItemIngredient;
+    // }
+    //
+    // public void Delete()
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"DELETE FROM menu_item_ingredients WHERE id = (@searchId);";
+    //   MySqlParameter searchId = new MySqlParameter();
+    //   searchId.ParameterName = "@searchId";
+    //   searchId.Value = this._id;
+    //   cmd.Parameters.Add(searchId);
+    //   cmd.ExecuteNonQuery();
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    // }
+    //
+    // public static void DeleteAll()
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"DELETE FROM menu_item_ingredients";
+    //   cmd.ExecuteNonQuery();
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    // }
+    //
+    // public void Edit(string newIngredientDescription, int newMenuItemsId, int newStoreId)
+    // {
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"UPDATE menu_item_ingredients
+    //                          SET ingredient_description = (@ingredientDescription),
+    //                              menu_items_id = (@menuItemsId),
+    //                             store_id = (@storeId)
+    //                        WHERE id = (@menuItemIngredientId);";
+    //   MySqlParameter ingredientDescriptionParameter = new MySqlParameter();
+    //   ingredientDescriptionParameter.ParameterName = "@ingredientDescription";
+    //   ingredientDescriptionParameter.Value = newIngredientDescription;
+    //   cmd.Parameters.Add(ingredientDescriptionParameter);
+    //   MySqlParameter menuItemsIdParameter = new MySqlParameter();
+    //   menuItemsIdParameter.ParameterName = "@menuItemsId";
+    //   menuItemsIdParameter.Value = newMenuItemsId;
+    //   cmd.Parameters.Add(ingredientDescriptionParameter);
+    //   MySqlParameter storeIdParameter = new MySqlParameter();
+    //   storeIdParameter.ParameterName = "@storeId";
+    //   storeIdParameter.Value = newStoreId;
+    //   cmd.Parameters.Add(storeIdParameter);
+    //   MySqlParameter menuItemIngredientIdParameter = new MySqlParameter();
+    //   menuItemIngredientIdParameter.ParameterName = "@menuItemIngredientId";
+    //   menuItemIngredientIdParameter.Value = this._id;
+    //   cmd.Parameters.Add(menuItemIngredientIdParameter);
+    //   cmd.ExecuteNonQuery();
+    //   _ingredientDescription = newIngredientDescription;
+    //   _menuItemsId = newMenuItemsId;
+    //   _storeId = newStoreId;
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    // }
+    //
+    // public override bool Equals(System.Object otherMenuItemIngredient)
+    // {
+    //   if (!(otherMenuItemIngredient is MenuItemIngredient))
+    //   {
+    //     return false;
+    //   }
+    //   else
+    //   {
+    //     MenuItemIngredient newMenuItemIngredient = (MenuItemIngredient) otherMenuItemIngredient;
+    //     bool idEquality = this.GetId().Equals(newMenuItemIngredient.GetId());
+    //     bool ingredientDescriptionEquality = this.GetIngredientDescription().Equals(newMenuItemIngredient.GetIngredientDescription());
+    //     bool menuItemsIdEquality = this.GetMenuItemsId().Equals(newMenuItemIngredient.GetMenuItemsId());
+    //     bool storeIdEquality = this.GetStoreId().Equals(newMenuItemIngredient.GetStoreId());
+    //     return (idEquality && ingredientDescriptionEquality && menuItemsIdEquality && storeIdEquality);
+    //   }
+    // }
   }
 }
