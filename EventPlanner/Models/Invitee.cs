109c757000ccb4622ca7dd3354474d93c34b49c9
Mark Strickland <mstrickland@globys.com>
2019-03-11 19:26:40 -0700

    Add Invitee DeleteAll() method and test.

--- EventPlanner/Models/Invitee.cs
+++ EventPlanner/Models/Invitee.cs
@@ -153,21 +153,21 @@ namespace EventPlanner.Models
       }
     }
 
-    // public static void DeleteAll()
-    // {
-    //   MySqlConnection conn = DB.Connection();
-    //   conn.Open();
-    //   var cmd = conn.CreateCommand() as MySqlCommand;
-    //   cmd.CommandText = @"DELETE FROM events_invitees;
-    //                       DELETE FROM invitees";
-    //   cmd.ExecuteNonQuery();
-    //   conn.Close();
-    //   if (conn != null)
-    //   {
-    //     conn.Dispose();
-    //   }
-    // }
-    //
+    public static void DeleteAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM events_invitees;
+                          DELETE FROM invitees";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
     // public void Edit(string newinviteeName, string newinviteeEmailAddress)
     // {
     //   MySqlConnection conn = DB.Connection();
