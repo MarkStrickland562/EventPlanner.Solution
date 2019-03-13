adcc19594ea1dd920cf9c3e992dee1e976569305
Guest User <Guest@epicodus-50.local>
2019-03-13 13:19:03 -0700

    discard system.linq (unneeded)

--- EventPlanner.Tests/ControllerTests/EventsControllerTests.cs
+++ EventPlanner.Tests/ControllerTests/EventsControllerTests.cs
@@ -2,7 +2,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using EventPlanner.Controllers;
 using EventPlanner.Models;
 
