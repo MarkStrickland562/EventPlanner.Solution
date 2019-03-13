a4dfcbbdd04d594789483d47d83c4157ee83617b
Guest User <Guest@epicodus-50.local>
2019-03-13 11:21:21 -0700

    add menuitemingredientscontroller tests

--- EventPlanner.Tests/ControllerTests/MenuItemIngredientsControllerTests.cs
+++ EventPlanner.Tests/ControllerTests/MenuItemIngredientsControllerTests.cs
@@ -13,6 +13,8 @@ namespace EventPlanner.Tests
     public MenuItemIngredientsControllerTest()
     {
       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=event_planner_tests;";
+      _controller = new MenuItemIngredientsController();
+      _menuItemIngredient = new MenuItemIngredient("TestDescription", 0, 0);
     }
     public void Dispose()
     {
@@ -25,5 +27,55 @@ namespace EventPlanner.Tests
       Task.ClearAll();
     }
 
+    private MenuItemIngredientsController _controller;
+    private MenuItemIngredient _menuItemIngredient;
+
+    [TestMethod]
+    public void Index_ReturnsCorrectView_True()
+    {
+      Assert.IsInstanceOfType(_controller.Index(), typeof(ViewResult));
+    }
+    [TestMethod]
+    public void Index_HasCorrectModelType_MenuItemIngredientsList()
+    {
+      ViewResult indexView = _controller.Index() as ViewResult;
+      var result = indexView.ViewData.Model;
+      Assert.IsInstanceOfType(result, typeof(List<MenuItemIngredient>));
+    }
+
+    [TestMethod]
+    public void New_ReturnCorrectViewOfForm_True()
+    {
+      ActionResult newView = _controller.New() as ActionResult;
+      Assert.IsInstanceOfType(newView, typeof(ViewResult));
+    }
+    [TestMethod]
+    public void Create_CreatesNewInstanceOfMenuItemIngredient_True()
+    {
+      ActionResult createPost = _controller.Create("TestDescription", 0, 0);
+      Assert.IsInstanceOfType(createPost, typeof(ActionResult));
+    }
+
+    [TestMethod]
+    public void Delete_DeletesMenuItemIngredientObject_True()
+    {
+      _menuItemIngredient.Save();
+      ActionResult deletePost = _controller.Delete(_menuItemIngredient.GetId());
+      Assert.IsInstanceOfType(deletePost, typeof(ActionResult));
+      CollectionAssert.AreEqual(new List<MenuItemIngredient> { }, MenuItemIngredient.GetAll());
+    }
+
+    [TestMethod]
+    public void Edit_HasCorrectModelType_MenuItemIngredientList()
+    {
+      Assert.IsInstanceOfType(_controller.Edit(_menuItemIngredient.GetId()), typeof(ViewResult));
+    }
+    [TestMethod]
+    public void Update_UpdatesInstanceOfMenuItemIngredient_True()
+    {
+      _menuItemIngredient.Save();
+      ActionResult updatePost = _controller.Update((_menuItemIngredient.GetId()), "NewDescription", 0, 0);
+      Assert.AreEqual("NewDescription", MenuItemIngredient.Find(_menuItemIngredient.GetId()).GetIngredientDescription());
+    }
   }
 }
