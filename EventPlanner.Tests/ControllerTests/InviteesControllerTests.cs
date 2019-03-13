90a70dca7e96bc84efc24bfdc54d1f251ad5c00c
Guest User <Guest@epicodus-50.local>
2019-03-13 11:01:18 -0700

    add inviteescontrollertests

--- EventPlanner.Tests/ControllerTests/InviteesControllerTests.cs
+++ EventPlanner.Tests/ControllerTests/InviteesControllerTests.cs
@@ -25,5 +25,70 @@ namespace EventPlanner.Tests
       Task.ClearAll();
     }
 
+    public InviteesController controller = new InviteesController();
+    public Invitee invitee = new Invitee("TestName", "Test@Email.com");
+
+    [TestMethod]
+    public void Index_ReturnsCorrectView_True()
+    {
+      Assert.IsInstanceOfType(controller.Index(), typeof(ViewResult));
+    }
+    [TestMethod]
+    public void Index_HasCorrectModelType_InviteesList()
+    {
+      ViewResult indexView = controller.Index() as ViewResult;
+      var result = indexView.ViewData.Model;
+      Assert.IsInstanceOfType(result, typeof(List<Invitee>));
+    }
+
+    [TestMethod]
+    public void New_ReturnCorrectViewOfForm_True()
+    {
+      ActionResult newView = controller.New() as ActionResult;
+      Assert.IsInstanceOfType(newView, typeof(ViewResult));
+    }
+    [TestMethod]
+    public void Create_CreatesNewInstanceOfInvitee_True()
+    {
+      ActionResult createPost = controller.Create("TestName", "Test@Email.com");
+      Assert.IsInstanceOfType(createPost, typeof(ActionResult));
+    }
+
+    [TestMethod]
+    public void AddEvent_AddInstanceOfEventForInvitee_True()
+    {
+      invitee.Save();
+      ActionResult showPost = controller.AddEvent(invitee.GetId(), 0);
+      Assert.IsInstanceOfType(showPost, typeof(ActionResult));
+    }
+    [TestMethod]
+    public void DeleteEvent_DeleteInstanceOfEventForInvitee_True()
+    {
+      invitee.Save();
+      ActionResult showPost = controller.DeleteEvent(invitee.GetId(), 0);
+      Assert.IsInstanceOfType(showPost, typeof(ActionResult));
+    }
+
+    [TestMethod]
+    public void Delete_DeletesInviteeObject_True()
+    {
+      invitee.Save();
+      ActionResult deletePost = controller.Delete(invitee.GetId());
+      Assert.IsInstanceOfType(deletePost, typeof(ActionResult));
+      CollectionAssert.AreEqual(new List<Invitee> { }, Invitee.GetAll());
+    }
+
+    [TestMethod]
+    public void Edit_HasCorrectModelType_InviteeList()
+    {
+      Assert.IsInstanceOfType(controller.Edit(invitee.GetId()), typeof(ViewResult));
+    }
+    [TestMethod]
+    public void Update_UpdatesInstanceOfInvitee_True()
+    {
+      invitee.Save();
+      ActionResult updatePost = controller.Update(invitee.GetId(), "NewName", "Test@Email.com");
+      Assert.AreEqual("NewName", Invitee.Find(invitee.GetId()).GetInviteeName());
+    }
   }
 }
