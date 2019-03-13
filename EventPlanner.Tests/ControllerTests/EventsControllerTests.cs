c9f3d9a159bb3c1ce3d28a8ecbdf216c7f08a60b
Guest User <Guest@epicodus-50.local>
2019-03-13 11:06:49 -0700

    secure/privatize global variables in eventscontrollertests

--- EventPlanner.Tests/ControllerTests/EventsControllerTests.cs
+++ EventPlanner.Tests/ControllerTests/EventsControllerTests.cs
@@ -14,6 +14,8 @@ namespace EventPlanner.Tests
     public EventsControllerTest()
     {
       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=event_planner_tests;";
+      _controller = new EventsController();
+      _newEvent = new Event("TestName", (new DateTime(2019, 12, 31, 21, 30, 0, DateTimeKind.Utc)), "TestLocation", 1);
     }
     public void Dispose()
     {
@@ -26,18 +28,18 @@ namespace EventPlanner.Tests
       Task.ClearAll();
     }
 
-    public EventsController controller = new EventsController();
-    public Event newEvent = new Event("TestName", (new DateTime(2019, 12, 31, 21, 30, 0, DateTimeKind.Utc)), "TestLocation", 1);
+    private EventsController _controller;
+    private Event _newEvent;
 
     [TestMethod]
     public void Index_ReturnsCorrectView_True()
     {
-      Assert.IsInstanceOfType(controller.Index(), typeof(ViewResult));
+      Assert.IsInstanceOfType(_controller.Index(), typeof(ViewResult));
     }
     [TestMethod]
     public void Index_HasCorrectModelType_EventsList()
     {
-      ViewResult indexView = controller.Index() as ViewResult;
+      ViewResult indexView = _controller.Index() as ViewResult;
       var result = indexView.ViewData.Model;
       Assert.IsInstanceOfType(result, typeof(List<Event>));
     }
@@ -45,21 +47,21 @@ namespace EventPlanner.Tests
     [TestMethod]
     public void New_ReturnCorrectViewOfForm_True()
     {
-      ActionResult newView = controller.New() as ActionResult;
+      ActionResult newView = _controller.New() as ActionResult;
       Assert.IsInstanceOfType(newView, typeof(ViewResult));
     }
     [TestMethod]
     public void Create_CreatesNewInstanceOfEvent_True()
     {
-      ActionResult createPost = controller.Create("TestName", (new DateTime(2019, 12, 31, 21, 30, 0, DateTimeKind.Utc)), "TestLocation", 1);
+      ActionResult createPost = _controller.Create("TestName", (new DateTime(2019, 12, 31, 21, 30, 0, DateTimeKind.Utc)), "TestLocation", 1);
       Assert.IsInstanceOfType(createPost, typeof(ActionResult));
     }
 
     // [TestMethod]
     // public void Show_ProcessesShowModelCorrectly_True()
     // {
-    //   newEvent.Save();
-    //   var showView = controller.Show(newEvent.GetId()) as ViewResult;
+    //   _newEvent.Save();
+    //   var showView = _controller.Show(_newEvent.GetId()) as ViewResult;
     //   var result = showView.ViewData.Model;
     //   Assert.AreEqual(result, typeof(Dictionary<string, object>));
     // }
@@ -67,38 +69,38 @@ namespace EventPlanner.Tests
     [TestMethod]
     public void AddTask_AddInstanceOfTaskForEvent_True()
     {
-      newEvent.Save();
-      ActionResult showPost = controller.AddTask(newEvent.GetId(), 0);
+      _newEvent.Save();
+      ActionResult showPost = _controller.AddTask(_newEvent.GetId(), 0);
       Assert.IsInstanceOfType(showPost, typeof(ActionResult));
     }
     [TestMethod]
     public void DeleteTask_DeleteInstanceOfTaskForEvent_True()
     {
-      newEvent.Save();
-      ActionResult showPost = controller.DeleteTask(newEvent.GetId(), 0);
+      _newEvent.Save();
+      ActionResult showPost = _controller.DeleteTask(_newEvent.GetId(), 0);
       Assert.IsInstanceOfType(showPost, typeof(ActionResult));
     }
 
     [TestMethod]
     public void AddInvitee_AddInstanceOfInviteeForEvent_True()
     {
-      newEvent.Save();
-      ActionResult showPost = controller.AddInvitee(newEvent.GetId(), 0);
+      _newEvent.Save();
+      ActionResult showPost = _controller.AddInvitee(_newEvent.GetId(), 0);
       Assert.IsInstanceOfType(showPost, typeof(ActionResult));
     }
     [TestMethod]
     public void DeleteInvitee_DeleteInstanceOfInviteeForEvent_True()
     {
-      newEvent.Save();
-      ActionResult showPost = controller.DeleteInvitee(newEvent.GetId(), 0);
+      _newEvent.Save();
+      ActionResult showPost = _controller.DeleteInvitee(_newEvent.GetId(), 0);
       Assert.IsInstanceOfType(showPost, typeof(ActionResult));
     }
 
     [TestMethod]
     public void Delete_DeletesEventObject_True()
     {
-      newEvent.Save();
-      ActionResult deletePost = controller.Delete(newEvent.GetId());
+      _newEvent.Save();
+      ActionResult deletePost = _controller.Delete(_newEvent.GetId());
       Assert.IsInstanceOfType(deletePost, typeof(ActionResult));
       CollectionAssert.AreEqual(new List<Event> { }, Event.GetAll());
     }
@@ -106,14 +108,14 @@ namespace EventPlanner.Tests
     [TestMethod]
     public void Edit_HasCorrectModelType_EventList()
     {
-      Assert.IsInstanceOfType(controller.Edit(newEvent.GetId()), typeof(ViewResult));
+      Assert.IsInstanceOfType(_controller.Edit(_newEvent.GetId()), typeof(ViewResult));
     }
     [TestMethod]
     public void Update_UpdatesInstanceOfEvent_True()
     {
-      newEvent.Save();
-      ActionResult updatePost = controller.Update(newEvent.GetId(), "NewName", (new DateTime(2019, 12, 31, 21, 30, 0, DateTimeKind.Utc)), "TestLocation", 1);
-      Assert.AreEqual("NewName", Event.Find(newEvent.GetId()).GetEventName());
+      _newEvent.Save();
+      ActionResult updatePost = _controller.Update(_newEvent.GetId(), "NewName", (new DateTime(2019, 12, 31, 21, 30, 0, DateTimeKind.Utc)), "TestLocation", 1);
+      Assert.AreEqual("NewName", Event.Find(_newEvent.GetId()).GetEventName());
     }
   }
 }
